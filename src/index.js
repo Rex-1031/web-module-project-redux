30c4705aa24dc1b4aa75ef15b88032261890867b
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2021-06-19 14:25:16 -0400

    setup redux steps complete

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+const store = createStore(reducer)
+
 ReactDOM.render(
   <Router>
-      <App />
-  </Router>,
+    <Provider store={store} ><App /></Provider>
+ </Router> ,
   document.getElementById('root')
 );
